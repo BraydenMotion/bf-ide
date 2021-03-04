a4be74d0d17b20704ec6637b340d2e641df95c8d
Brayden <braydenmotion12@gmail.com>
2021-03-04 07:07:36 -0600

    Add Kotest to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("jvm") version "1.4.21"
 }
 
+val kotestVersion: String by project
+
 group = "io.bmotion"
 version = "1.0-SNAPSHOT"
 
@@ -11,9 +13,16 @@ repositories {
     mavenCentral()
 }
 
-dependencies {}
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
 
 tasks.withType<KotlinCompile>() {
     // will add tornadofx which needs java 8
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
